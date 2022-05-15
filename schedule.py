0d08ccba35313b3581904a96eedc1449d13716b6
CodeATA <cdwdhr@gmail.com>
2022-05-15 17:42:13 +0800

    分离指令处理过程

--- schedule.py
+++ schedule.py
@@ -21,75 +21,97 @@ cancel_pat = re.compile(r"/取消直播 (?P<sch_id>.*)")
 split_pat = re.compile(r";|；")
 time_pat = re.compile(r"(?P<mon>[0-9][0-9]?)-(?P<day>[0-9][0-9]?)\s+(?P<hour>[0-9][0-9]?)(:(?P<min>[0-9][0-9]?))?")
 
+def process_command(command: str):
+    res = {}
+    res['stat'] = 0
+
+    command_split = re.split(split_pat, command, maxsplit=3)
+    res['title'] = command_split[0]
+    start_time = command_split[1]
+    end_time = command_split[2]
+    description = ''
+    if len(command_split) > 3:
+            description = command_split[3]
+    
+    start_time = start_time.replace('：', ':')
+    end_time = end_time.replace('：', ':')
+    cur_time = time.localtime()
+    m_start = re.match(time_pat, start_time)
+    m_end = re.match(time_pat, end_time)
+    if m_start:
+        start_mon = int(m_start.group('mon'))
+        start_day = int(m_start.group('day'))
+        start_hour = int(m_start.group('hour'))
+        if m_start.group('min') == None:
+            start_min = int(0)
+        else:
+            start_min = int(m_start.group('min'))
+    else:
+        res['stat'] = 1 # 起始时间格式错误
+        return res
+    
+    if m_end:
+        end_mon = int(m_end.group('mon'))
+        end_day = int(m_end.group('day'))
+        end_hour = int(m_end.group('hour'))
+        if m_end.group('min') == None:
+            end_min = int(0)
+        else:
+            end_min = int(m_end.group('min'))
+    else:
+        res['stat'] = 2 # 结束时间格式错误
+        return res
+
+    if start_mon < cur_time.tm_mon:
+        start_year = cur_time.tm_year + 1
+    else:
+        start_year = cur_time.tm_year
+    if end_mon < cur_time.tm_mon:
+        end_year = cur_time.tm_year + 1
+    else:
+        end_year = cur_time.tm_year
+    start_time_struct = time.strptime(f"{start_year}-{start_mon}-{start_day} {start_hour}:{start_min}", '%Y-%m-%d %H:%M')
+    end_time_struct = time.strptime(f"{end_year}-{end_mon}-{end_day} {end_hour}:{end_min}", '%Y-%m-%d %H:%M')
+    start_timestamp = int(time.mktime(start_time_struct) * 1000)
+    end_timestamp = int(time.mktime(end_time_struct) * 1000)
+
+    if start_timestamp > end_timestamp:
+        res['stat'] = 3 # 结束时间需要晚于起始时间
+        return res
+    
+    res['description'] = description
+    res['start_timestamp'] = str(start_timestamp)
+    res['end_timestamp'] = str(end_timestamp)
+
+    return res
+
 async def _schedule_handler(context: WsContext, message: qqbot.Message):
     # msg_api = qqbot.AsyncMessageAPI(t_token, False)
     dmsg_api = qqbot.AsyncDmsAPI(t_token, False)
     schedule_api = qqbot.AsyncScheduleAPI(t_token, False)
+    channel_api = qqbot.AsyncChannelAPI(t_token, False)
 
     qqbot.logger.info("event_type %s" % context.event_type + ",receive message %s" % message.content)
 
     if message.content.startswith("/预约直播"):
-        command_str = re.match(appoint_pat, message.content).group('command')
-        command_split = re.split(split_pat, command_str, maxsplit=3)
+        # command_str = re.match(appoint_pat, message.content).group('command')
+        processed_sch = process_command(re.match(appoint_pat, message.content).group('command'))
 
-        title = command_split[0]
-        start_time = command_split[1]
-        end_time = command_split[2]
-        description = ''
-        if len(command_split) > 3:
-            description = command_split[3]
-
-        start_time = start_time.replace('：', ':')
-        end_time = end_time.replace('：', ':')
-        cur_time = time.localtime()
-        m_start = re.match(time_pat, start_time)
-        m_end = re.match(time_pat, end_time)
-        if m_start:
-            start_mon = int(m_start.group('mon'))
-            start_day = int(m_start.group('day'))
-            start_hour = int(m_start.group('hour'))
-            if m_start.group('min') == None:
-                start_min = int(0)
-            else:
-                start_min = int(m_start.group('min'))
-        else:
+        if processed_sch['stat'] == 1:
             message_to_send = qqbot.MessageSendRequest(
                 content="起始时间格式错误！",
                 msg_id=message.id
                 )
             await dmsg_api.post_direct_message(message.guild_id, message_to_send)
             return
-
-        if m_end:
-            end_mon = int(m_end.group('mon'))
-            end_day = int(m_end.group('day'))
-            end_hour = int(m_end.group('hour'))
-            if m_end.group('min') == None:
-                end_min = int(0)
-            else:
-                end_min = int(m_end.group('min'))
-        else:
+        elif processed_sch['stat'] == 2:
             message_to_send = qqbot.MessageSendRequest(
                 content="结束时间格式错误！",
                 msg_id=message.id
                 )
             await dmsg_api.post_direct_message(message.guild_id, message_to_send)
             return
-
-        if start_mon < cur_time.tm_mon:
-            start_year = cur_time.tm_year + 1
-        else:
-            start_year = cur_time.tm_year
-        if end_mon < cur_time.tm_mon:
-            end_year = cur_time.tm_year + 1
-        else:
-            end_year = cur_time.tm_year
-        start_time_struct = time.strptime(f"{start_year}-{start_mon}-{start_day} {start_hour}:{start_min}", '%Y-%m-%d %H:%M')
-        end_time_struct = time.strptime(f"{end_year}-{end_mon}-{end_day} {end_hour}:{end_min}", '%Y-%m-%d %H:%M')
-        start_timestamp = int(time.mktime(start_time_struct) * 1000)
-        end_timestamp = int(time.mktime(end_time_struct) * 1000)
-
-        if start_timestamp > end_timestamp:
+        elif processed_sch['stat'] == 3:
             message_to_send = qqbot.MessageSendRequest(
                 content="结束时间需晚于起始时间！",
                 msg_id=message.id
@@ -98,14 +120,13 @@ async def _schedule_handler(context: WsContext, message: qqbot.Message):
             return
 
         to_create = qqbot.ScheduleToCreate(
-            name=title,
-            description=description,
-            start_timestamp = str(start_timestamp),
-            end_timestamp = str(end_timestamp),
+            name = processed_sch['title'],
+            description = processed_sch['description'],
+            start_timestamp = processed_sch['start_timestamp'],
+            end_timestamp = processed_sch['end_timestamp'],
             remind_type="0",
         )
-        schedule_api = qqbot.ScheduleAPI(t_token, False)
-        schedule = schedule_api.create_schedule(
+        schedule = await schedule_api.create_schedule(
             POT_SCHEDULE_ID,
             to_create
         )
@@ -117,17 +138,22 @@ async def _schedule_handler(context: WsContext, message: qqbot.Message):
         另一个蛋疼的地方是获取日程列表的API只能获取当天的日程，所以只能把日程ID附在描述里，否则找不到正确的日程。
         '''
         to_patch = qqbot.ScheduleToPatch(
-            name=title,
-            description=description + f"\n\n{schedule.id}",
-            start_timestamp = str(start_timestamp),
-            end_timestamp = str(end_timestamp),
+            name = processed_sch['title'],
+            description = processed_sch['description'] + f"\n\n{schedule.id}",
+            start_timestamp = processed_sch['start_timestamp'],
+            end_timestamp = processed_sch['end_timestamp'],
             remind_type="0",
         )
-        patched_schedule = schedule_api.update_schedule(
+        patched_schedule = await schedule_api.update_schedule(
             POT_SCHEDULE_ID,
             schedule.id,
             to_patch
         )
+        ret_message = qqbot.MessageSendRequest(
+            content=f'日程创建成功，ID：{patched_schedule.id}',
+            msg_id=message.id
+        )
+        await dmsg_api.post_direct_message(message.guild_id, ret_message)
 
     elif message.content.startswith("/取消直播"):
         sch_id = re.match(cancel_pat, message.content).group('sch_id')
@@ -141,9 +167,24 @@ async def _schedule_handler(context: WsContext, message: qqbot.Message):
             return
         await schedule_api.delete_schedule(POT_SCHEDULE_ID, sch_id)
 
+    elif message.content.startswith("/频道列表"):
+        channels = await channel_api.get_channels(GUILD_ID)
+        msg_content = ''
+        for ch in channels:
+            if ch.name != '':
+                msg_content += f"{ch.name}, {ch.id}\n"
+        message_to_send = qqbot.MessageSendRequest(
+            content=msg_content,
+            msg_id=message.id
+        )
+        await dmsg_api.post_direct_message(message.guild_id, message_to_send)
+    
+    elif message.content.startswith("/绑定点歌"):
+        return
+
     else:
         message_to_send = qqbot.MessageSendRequest(
-            content='/预约直播 标题;开始时间(mm-dd HH:MM);结束时间(mm-dd HH:MM);描述(可选)\n/取消直播 日程ID',
+            content='指令列表：\n/预约直播 标题;开始时间(mm-dd HH:MM);结束时间(mm-dd HH:MM);描述(可选)\n/取消直播 日程ID\n/更新直播 日程ID;\n/频道列表\n/绑定点歌 频道ID（未实装，仅监听频道消息）',
             msg_id=message.id
             )
         await dmsg_api.post_direct_message(message.guild_id, message_to_send)
